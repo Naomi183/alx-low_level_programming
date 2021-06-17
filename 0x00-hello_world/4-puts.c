2be818fa25b5c87fa2bf1fe241ef941fe7168fb2
Ufuoma Okorodudu <naomiokorodudu9@gmail.com>
2021-06-17 11:27:22 -0700

    A programming commit in 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- *main - Entry point
- *
- *Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-  puts("\"
+       puts("\"
 Programming is like building a multilingual puzzle");
-  return (0);
+return (0);
 }
+
